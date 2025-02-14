1f75dee765462b79e463f5be006d34c7534ddaa5
ferclima05 <fernandocl3@al.insper.edu.br>
2025-02-14 15:12:22 -0300

    v0.2

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,7 +1,7 @@
 #include "asf.h"
 
 volatile int g_cnt = 0;
-volatile char g_str[10];
+//volatile char g_str[10];
 //int batata;
 
 // This code creates a progress bar on an OLED screen that
@@ -12,16 +12,16 @@ void btn_callback(void) {
   //if (g_cnt >= 8)
   //  g_cnt = 0;
 
-  g_cnt = 2;
+  g_cnt = 1;
 
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
+  //int i = 0;
+  //for (i = 0; i < g_cnt; i++) {
+    //g_str = 1;
+    //g_str[i] = '*';
+    //g_str[i + 1] = NULLL;
     //delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+    //gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+  //}
 }
 
 void main(void) {
